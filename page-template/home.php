a4308926b0e0bb5c41812f9e0c2f145eb88543fd
sanjayrabadiya <sanjayrabadiya30@gmail.com>
2023-03-15 09:16:46 +0530

    upload code

---
+++ page-template/home.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Home Page Template
+ * Template Post Type: page
+ */
+
+get_header();
+?>
+
+<main>
+<?php the_content(); ?> 
+
+</main>
+
+<?php get_footer(); ?>
+
+
---
+++ page-template/news.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: News Page Template
+ * Template Post Type: page
+ */
+
+get_header();
+?>
+
+<main>
+	
+</main>
+
+<?php get_footer(); ?>
+
+
